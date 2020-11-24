5be9802cea1e797fc348e8738538bd3f6c6c569b
Shashank Shukla <shashankshukla96@gmail.com>
2020-11-24 19:44:42 +0530

    changed DB to atlas

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 const config = {
     DB: {
-        CONNECTION_URI: "mongodb://127.0.0.1:27017/got-battle",
+        CONNECTION_URI: "mongodb+srv://career-ninja:got-battle@cluster0.j2aoc.mongodb.net/got-battle",
     },
 };
 
